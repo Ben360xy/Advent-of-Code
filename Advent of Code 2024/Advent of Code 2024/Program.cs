bc34624cd88dfe497aa07b3b91fa53891bfddd7b
Ben360xy <ben.r.victor@gmail.com>
2024-12-19 10:28:17 -0500

    day1 and half of day 2

---
+++ Advent of Code 2024/Advent of Code 2024/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Advent_of_Code_2024;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string filePath = "C:\\Users\\benrv\\source\\repos\\Advent of Code 2024\\Advent of Code 2024\\Data\\input.txt";
+
+        string[] lines = File.ReadAllLines(filePath);
+
+        int[] column1 = new int[lines.Length];
+        int[] column2 = new int[lines.Length];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string[] numbers = lines[i].Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (numbers.Length == 2)
+            {
+                column1[i] = int.Parse(numbers[0]);
+                column2[i] = int.Parse(numbers[1]);
+            }
+        }
+
+        Array.Sort(column1);
+        Array.Sort(column2);
+
+        int total = 0;
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            total += Math.Abs(column1[i] - column2[i]);
+        }
+        Console.WriteLine("part 1: " + total);
+
+        int total2 = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            int apperences = 0;
+            for (int j = 0; j < column1.Length; j++)
+            {
+                if (column1[i] == column2[j])
+                {
+                    apperences++;
+                }
+            }
+            total2 += column1[i] * apperences;
+        }
+
+        Console.WriteLine("part 2; " + total2);
+    }
+}
---
+++ Advent of Code 2024/Advent_of_Code_2024(2)/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Advent_of_Code_2024;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string filePath =
+            "C:\\Users\\benrv\\source\\repos\\Advent of Code 2024\\Advent_of_Code_2024(2)\\Data\\input.txt";
+
+        string[] lines = File.ReadAllLines(filePath);
+
+
+
+        int[][] input = lines
+            .Select(row => row
+                .Split(' ')              
+                .Select(int.Parse)      
+                .ToArray())             
+            .ToArray();
+
+        int output = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i][0] > input[i][1])
+            {
+                output += decrease(input[i]);
+            }
+            else if (input[i][0] < input[i][1])
+            {
+                output += increase(input[i]);
+            }
+        }
+        Console.WriteLine(output);
+    }
+
+    public static int decrease(int[] num)
+    {
+        for (int i = 0; i < num.Length - 1; i++)
+        {
+            int dif = num[i] - num[i + 1];
+            if (dif < 1 || dif > 3)
+            {
+                if (safetyCheckD(num) == 0);
+                {
+                    return 0;
+                }
+                
+            }
+        }
+        return 1;
+    }
+    public static int increase(int[] num)
+    {
+        for (int i = 0; i < num.Length - 1; i++)
+        {
+            int dif = num[i + 1] - num[i];
+            if (dif < 1 || dif > 3)
+            {
+                if (safetyCheckI(num) == 0) ;
+                {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+
+    public static int safetyCheckD(int[] num)
+    {
+
+    }
+    public static int safetyCheckI(int[] num)
+    {
+
+    }
+}
